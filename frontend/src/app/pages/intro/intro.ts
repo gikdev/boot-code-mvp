d069c7d4b0a7842e67ac022b8ee35d2d1a0c2122
Bahrami <m.m.bhrm85@gmail.com>
2025-12-15 22:55:52 +0330

    Install `spartan.ng` (Angular's `@shadcn/ui`)

--- frontend/src/app/pages/intro/intro.ts
+++ frontend/src/app/pages/intro/intro.ts
@@ -1,14 +1,14 @@
 import { Component } from "@angular/core"
+import { HlmButtonImports } from "@spartan-ng/helm/button"
 import { TestNav } from "../../test-nav"
 
 @Component({
     selector: "app-intro",
-    imports: [TestNav],
+    imports: [TestNav, HlmButtonImports],
     template: `
-        <p>
-        intro works!
-        </p>
+        <p>intro works!</p>
         <app-test-nav />
+        <button hlmBtn>Hello world!</button>
     `,
     styles: ``,
 })
