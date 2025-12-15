6502e3f09b320490fce47bab9ebb7a3f31bba358
Bahrami <m.m.bhrm85@gmail.com>
2025-12-16 02:41:02 +0330

    Test Angular http client

--- backend/src/Backend.Api/Program.cs
+++ backend/src/Backend.Api/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors(o => {
+    o.AllowAnyHeader();
+    o.AllowAnyMethod();
+    o.AllowAnyOrigin();
+});
+
+app.MapGet("/", () => new { message = "Hello World!" });
 
 app.Run();
--- frontend/src/app/pages/intro/intro.ts
+++ frontend/src/app/pages/intro/intro.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core"
+import { HttpClient } from "@angular/common/http"
+import { Component, DestroyRef, inject, signal } from "@angular/core"
 import { HlmButtonImports } from "@spartan-ng/helm/button"
 import { TestNav } from "../../test-nav"
 
@@ -8,8 +9,56 @@ import { TestNav } from "../../test-nav"
     template: `
         <p>intro works!</p>
         <app-test-nav />
-        <button hlmBtn>Hello world!</button>
+        <button hlmBtn (click)="callServer()">Hello world!</button>
+        @let api = apiState();
+
+        @switch (api.status) {
+            @case ('idle') {
+                <p>Idle.</p>
+            }
+
+            @case ('loading') {
+                <p>Loading...</p>
+            }
+
+            @case ('error') {
+                <p>Something went wrong: {{ api.message }}</p>
+            }
+
+            @case ('success') {
+                <p>The server says: {{ api.data.message }}</p>
+            }
+
+            @default {
+                <div>nothing...</div>
+            }
+        }
     `,
     styles: ``,
 })
-export class Intro {}
+export class Intro {
+    protected apiState = signal<ApiState>({ status: "idle" })
+    private readonly _httpClient = inject(HttpClient)
+    private readonly _destroyRef = inject(DestroyRef)
+
+    protected callServer() {
+        this.apiState.set({ status: "loading" })
+
+        const subscription = this._httpClient.get<MessageResponse>("http://localhost:5125/").subscribe({
+            next: (val: MessageResponse) => this.apiState.set({ status: "success", data: val }),
+            error: err => this.apiState.set({ status: "error", message: err.message }),
+        })
+
+        this._destroyRef.onDestroy(() => subscription.unsubscribe())
+    }
+}
+
+type ApiState =
+    | { status: "idle" }
+    | { status: "error"; message: string }
+    | { status: "loading" }
+    | { status: "success"; data: MessageResponse }
+
+interface MessageResponse {
+    message: string
+}
