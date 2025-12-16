4229d7aa2a2c16c7de61bcb11c512727955145c6
gikdev <m.m.bhrm85@gmail.com>
2025-12-16 13:40:50 +0330

    Install some packages & config ports.
    Angular: 4263
    Backend HTTP: 5263
    Backend HTTPS: 7263

--- backend/src/Backend.Api/Program.cs
+++ backend/src/Backend.Api/Program.cs
@@ -1,20 +1,25 @@
 using FastEndpoints;
 
+// ---
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddOpenApi();
 builder.Services.AddFastEndpoints();
 builder.Services.AddCors();
 
-
+// ---
 
 var app = builder.Build();
 
-app.UseCors(o => {
-    o.AllowAnyHeader();
-    o.AllowAnyMethod();
-    o.AllowAnyOrigin();
-});
+app.UseCors(o => o
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin()
+);
 
 app.MapFastEndpoints();
+
+// ---
 
 app.Run();
