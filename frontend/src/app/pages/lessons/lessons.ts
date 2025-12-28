e6fc003500ba8e6d3e742828e8d405b6aff240a3
gikdev <m.m.bhrm85@gmail.com>
2025-12-28 19:23:54 +0330

    Make `/lessons/:lessonId` load the lesson & show the title.

--- frontend/src/app/pages/lessons/lessons.ts
+++ frontend/src/app/pages/lessons/lessons.ts
@@ -1,17 +1,31 @@
 import { Component, inject, signal } from "@angular/core"
 import { ActivatedRoute } from "@angular/router"
-import { TestNav } from "../../test-nav"
+import { NgIcon, provideIcons } from "@ng-icons/core"
+import { phosphorArrowCounterClockwiseFill } from "@ng-icons/phosphor-icons/fill"
+import { phosphorSpinnerGap } from "@ng-icons/phosphor-icons/regular"
+import { injectQuery } from "@tanstack/angular-query-experimental"
+import { getLessonByIdOptions } from "#/api/generated/client"
+import { HlmButtonImports } from "#/libs/ui/button/src"
 
 @Component({
   selector: "app-lesson",
-  imports: [TestNav],
-  template: `
-    <app-test-nav />
-    <p> Lesson works! #{{ lessonId() }} </p>
-  `,
+  imports: [HlmButtonImports, NgIcon],
+  viewProviders: [
+    provideIcons({ phosphorArrowCounterClockwiseFill, phosphorSpinnerGap }),
+  ],
+  templateUrl: "./lessons.html",
 })
 export class Lessons {
-  lessonId = signal("")
+  lessonId = signal<string | null>(null)
+  lessonQuery = injectQuery(() => ({
+    ...getLessonByIdOptions({
+      path: {
+        // biome-ignore lint/style/noNonNullAssertion: handled by Tanstack Query.
+        id: this.lessonId()!,
+      },
+    }),
+    enabled: () => isStringWithContent(this.lessonId()),
+  }))
 
   private readonly activatedRoute = inject(ActivatedRoute)
 
@@ -22,7 +36,13 @@ export class Lessons {
   private handleParams() {
     this.activatedRoute.params.subscribe(params => {
       // biome-ignore lint/complexity/useLiteralKeys: I'd get TS error otherwise!
-      this.lessonId.set(params["lessonId"])
+      const lessonId = params["lessonId"]
+      if (!isStringWithContent(lessonId)) return
+      this.lessonId.set(lessonId)
     })
   }
+}
+
+function isStringWithContent(value: unknown): value is string {
+  return typeof value === "string" && value.trim().length > 0
 }
