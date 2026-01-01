2fb37c9ed7a606a32511159c24fb072b3e61cedd
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2026-01-01 21:03:25 +0330

    Fix problems.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,5 @@
 import { bootstrapApplication } from "@angular/platform-browser"
 import { App } from "./app/app"
 import { appConfig } from "./app/app.config"
-import "@phosphor-icons/web/regular"
-import "@phosphor-icons/web/fill"
 
 bootstrapApplication(App, appConfig).catch(console.error)
